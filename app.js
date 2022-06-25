4f0aee0295182a493399042ad36b78422dba54bc
lucaspineda <lucas.pineda@hotmail.com>
2022-06-25 19:19:30 -0300

    routing requests

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const http = require('http')
 
 function rqListener(req, res) {
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
+    res.write('</html>')    
+    return res.end();     
+  }
   console.log(req.url, req.method, req.headers)
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>heello</html>')
