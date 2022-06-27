5e9795d512e2ff824342780fe44ca844d0c6eb7f
lucaspineda <lucas.pineda@hotmail.com>
2022-06-27 11:47:46 -0300

    redirecting request

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http')
+const fs = require('fs')
 
 function rqListener(req, res) {
   const url = req.url;
+  const method = req.method;
+
   if (url === '/') {
     res.write('<html>')
     res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
     res.write('</html>')    
     return res.end();     
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY')
+    res.StatusCode = 302;
+    res.setHeader('Location', '/')
+    return res.end();
   }
   console.log(req.url, req.method, req.headers)
   res.setHeader('Content-Type', 'text/html')
