3cdfc1ead104f3a483569d665a8811e519be4f91
lucaspineda <lucas.pineda@hotmail.com>
2022-07-04 00:25:02 -0300

    create 404 page

--- express/app.js
+++ express/app.js
@@ -10,5 +10,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('Page not found')
+})
+
 
 app.listen(3000)
