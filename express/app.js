482d1ee85f81c8154556a8f8c03291f6d9c81810
lucaspineda <lucas.pineda@hotmail.com>
2022-07-04 09:34:00 -0300

    filtering paths

--- express/app.js
+++ express/app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop.js')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('in the middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body, 'body')
     res.redirect('/')
 });
