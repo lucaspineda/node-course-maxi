0786b0266acb2b92fe51ef90937972738200e946
lucaspineda <lucas.pineda@hotmail.com>
2022-09-12 00:14:27 -0300

    add login route

---
+++ 01-creating-the-login-form/app.js
@@ -0,0 +1,66 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+console.log('foiiiii')
+
+    // app.listen(3001);
+
+
+mongoose
+  .connect(
+    'mongodb+srv://lucas:lucas7796@clusterlucas.fbomq.azure.mongodb.net/ClusterLucas?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'lucas',
+          email: 'lucas.pineda@hotmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3001);
+    console.log('conectouu')
+
+  })
+  .catch(err => {
+    console.log(err);
+    console.log('errorrr')
+  });
---
+++ 01-creating-the-login-form/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+console.log('foi rota')
+
+router.get('/login', authController.getLogin);
+
+router.post('/login', authController.postLogin);
+
+module.exports = router;
